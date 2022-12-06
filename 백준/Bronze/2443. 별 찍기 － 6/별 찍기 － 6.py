a54a017f359c38a0e2296233c7d48f98b9040cb3
ssuzzang <97435321+ssuzzang@users.noreply.github.com>
2022-12-06 17:59:12 +0900

    전체 코드 업로드 -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.py
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.py
@@ -1,3 +1,4 @@
 n = int(input())
 for i in range(n, 0, -1):
-    print(' '*(n-i) + '*'*(2*i-1))
+    a = (' '*(n-i) + '*'*(2*i-1))
+    print(a)
--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,5 +1,7 @@
 n = int(input())
 for i in range(1, n+1):
-    print(' '*(n-i)+'*'*(2*i-1))
+    a = (' '*(n-i)+'*'*(2*i-1))
+    print(a)
 for i in range(n-1, 0, -1):
-    print(' '*(n-i)+'*'*(2*i-1))
+    a = (' '*(n-i)+'*'*(2*i-1))
+    print(a)
